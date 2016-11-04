1162589be6ec3bef831310dbf9c3c79efd784569
Jmoreno1988 <josemorenocasero@hotmail.com>
2016-11-04 23:26:56 +0100

    Changes, changes, changes!!!

---
+++ cliente/plataformaWeb/app/cfg.js
@@ -0,0 +1,5 @@
+﻿var cfg = {
+    variants: ["Standard", "CrazyHouse", "Chess960", "King of the Hill", "Atomic"],
+    timeControl: ["5 min", "1 hour", "1 day", "unlimited"],
+    colors: ["White", "Black", "Random"]
+}
